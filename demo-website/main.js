568e0897fb986628951455d362c186bc54fc163a
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2023-10-31 09:56:04 +0800

    Update demo website

--- demo-website/main.js
+++ demo-website/main.js
@@ -1,9 +1,28 @@
 // main.js
-document.getElementById('submit').addEventListener('click', function() {
+function strategyChange() {
+    let strategy = document.getElementById('strategy').value;
+    let timeQuantumDiv = document.getElementById('timeQuantumDiv');
+
+    // If strategy is RR, show Time Quantum input. Otherwise, hide it.
+    if(strategy === 'RR') {
+        timeQuantumDiv.style.display = 'block';
+    } else {
+        timeQuantumDiv.style.display = 'none';
+    }
+}
+
+function submitClick() {
+    let strategy = document.getElementById('strategy').value;
     let arrivalTime = document.getElementById('arrivalTime').value;
     let burstTime = document.getElementById('burstTime').value;
+    let timeQuantum = document.getElementById('timeQuantum').value;
+    let output = 'Strategy: ' + strategy + '\nArrival Time: ' + arrivalTime + '\nBurst Time: ' + burstTime;
 
-    // Print the values in the console
-    console.log(`Arrival Time: ${arrivalTime}`);
-    console.log(`Burst Time: ${burstTime}`);
-});
+    // If strategy is RR, add Time Quantum to output.
+    if(strategy === 'RR') {
+        output += '\nTime Quantum: ' + timeQuantum;
+    }
+
+    // Output the results in the textbox
+    document.getElementById('output').innerText = output;
+}
