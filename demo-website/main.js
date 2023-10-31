f46770ad9d3ca2391cc90dcbbd32614ff4d94938
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2023-10-31 13:57:57 +0800

    Complete demo website

--- demo-website/main.js
+++ demo-website/main.js
@@ -25,4 +25,32 @@ function submitClick() {
 
     // // Output the results in the textbox
     // document.getElementById('output').innerText = output;
+
+    // let encodedStr = encodeURIComponent(document.getElementById('output').value);
+    // let dataUri = `data:text/plain;charset=utf-8,${encodedStr}`
+    let downloadLink = document.getElementById('download-link');
+    // downloadLink.setAttribute('href', dataUri);
+    downloadLink.style.display = 'block';
 }
+
+function downloadFile() {
+    let textFileContent = document.getElementById('output').value;
+
+    let textFileAsBlob = new Blob([textFileContent], {type:'text/plain'});
+    let anchor = document.createElement('a');
+    anchor.href = URL.createObjectURL(textFileAsBlob);
+    anchor.download = 'output.txt';
+    anchor.click();
+}
+
+document.getElementById('compare').addEventListener('click', function() {
+    let output = document.getElementById('output').value;
+    let comparisonText = document.getElementById('comparison-box').value;
+
+    if(output === comparisonText) {
+        alert('Output MATCHES the comparison text!');
+    }
+    else {
+        alert('Output DOES NOT MATCH the comparison text!')
+    }
+});
--- demo-website/main.py
+++ demo-website/main.py
@@ -1,16 +1,56 @@
+from enum import IntEnum
+
 from pyscript import document
 
-# from MP1 import test
+from MP1 import Process, format_result, solve
+
+
+class Algorithm(IntEnum):
+    FCFS = 0
+    SJF = 1
+    SRTF = 2
+    RR = 3
 
 
 def simulate(event):
-    print("wow")
+    print("Simulate button clicked")
+
     arrival_time = document.querySelector("#arrivalTime").value
     burst_time = document.querySelector("#burstTime").value
-    time_quantum = document.querySelector("#timeQuantum").value
-    algorithm = document.querySelector("#algorithm").value
+    arrival_time = [int(x) for x in arrival_time.split(" ")]
+    burst_time = [int(x) for x in burst_time.split(" ")]
+    print(arrival_time)
+    print(burst_time)
+
+    if len(arrival_time) != len(burst_time):
+        print(
+            "Error: Arrival time and burst time must have the same number of processes"
+        )
+        print(f"Arrival time: {len(arrival_time)}")
+        print(f"Burst time: {len(burst_time)}")
+        return None
+    print("No error")
+
+    processes = []
+    process_count = len(arrival_time)
+    for i in range(process_count):
+        print(i + 1, arrival_time[i], burst_time[i])
+        processes.append(Process(i + 1, arrival_time[i], burst_time[i]))
+    algorithm = Algorithm[document.querySelector("#algorithm").value].value
+    print(algorithm)
+
+    time_quantum = 1
+    if algorithm == Algorithm.RR.value:
+        time_quantum = int(document.querySelector("#timeQuantum").value)
+        print(time_quantum)
+        if time_quantum == "":
+            print("Error: Time quantum must be set for Round Robin algorithm")
+            return None
+
+    time_slices = solve(algorithm, processes, int(time_quantum))
+    result = format_result(time_slices, process_count)
 
     output = document.querySelector("#output")
-    output.innerHTML = f"wow\n{algorithm}\n{arrival_time}\n{burst_time}\n{time_quantum}"
+    output.innerHTML = result
 
-    # print(test())
+    return None
